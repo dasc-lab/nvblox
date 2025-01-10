1029cd7d19f8bba07902be8c405e6549f9ec7b10
Trushant Adeshara <trushant@umich.edu>
2025-01-10 01:32:20 -0500

    updated _replica_eval.sh

--- nvblox/_replica_eval.sh
+++ nvblox/_replica_eval.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-# Define the directories
-EVAL_RES_DIR="./eval_results"
-
-create_eval_directory_structure() {
-    echo "Creating eval directory strucutre..."
-    mkdir -p "$EVAL_RES_DIR"
-    echo "Result directory created successfully."
-}
-
-# Check if the result_eval already exists
-if [ -d "$EVAL_RES_DIR" ]; then
-    # Get the current timestamp
-    TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-
-    # Create a backup of the existing directory
-    BACKUP_DIR="${EVAL_RES_DIR}_backup_$TIMESTAMP"
-    echo "Backing up existing $EVAL_RES_DIR to $BACKUP_DIR..."
-    mv "$EVAL_RES_DIR" "$BACKUP_DIR"
-    echo "Backup created successfully."
-fi
-
-# Create the new directory structure
-create_eval_directory_structure
+EVAL_RES_DIR=./eavl_results
 
 # Run fuse_replica on office0 dataset
 echo "Running fuse_replica on office0"
@@ -31,12 +9,10 @@ echo "Running fuse_replica on office0"
 BUILD_DIR="./build/executables"
 DATASET_DIR="$HOME/data/Replica/office0"
 
-# python3 color_toggle_viz.py ./office0_mesh.ply ./results_eval/final_result/transformed_certi_mesh.ply
-
 
 "${BUILD_DIR}/fuse_replica" "$DATASET_DIR" \
     --output_dir_path="$EVAL_RES_DIR" \
-    --working_mode="HEURISTIC"\
+    --working_mode="BASELINE"\
     --voxel_size=0.02 \
     --num_frames=2000 \
     --standard_deviation=1 \
